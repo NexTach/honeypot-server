673252cd7bd771aeadbe0be09f08aba01c3574b1
snowykte0426 <snowykte0426@naver.com>
2026-06-19 13:38:17 +0900

    init(global): 레포지터리 설정 및 생성

---
+++ src/main/kotlin/team/themoment/honeypotserver/HoneypotServerApplication.kt
@@ -0,0 +1,11 @@
+package team.themoment.honeypotserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HoneypotServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<HoneypotServerApplication>(*args)
+}
